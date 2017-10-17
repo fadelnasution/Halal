0f4fcf67034f3b798d0dc0d04479425ab0c3eefe
Fadel Nasution <fadelnasution@id-bin-14-kevinch.local>
2017-10-17 20:59:05 +0700

    Initial Commit

---
+++ projectMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectMap
+//
+//  Created by Fadel Muhammad Nasution on 10/17/17.
+//  Copyright © 2017 Binus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
